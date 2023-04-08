46934cae2be31ff3f79114d3165df3e7de373430
Александр <sawa20052@yandex.ru>
2023-04-08 18:16:03 +0300

    правки

--- laba4/src/main/java/ru/muctr/part2/Demo.java
+++ laba4/src/main/java/ru/muctr/part2/Demo.java
@@ -1,19 +1,22 @@
-package ru.muctr.part2;;
+package ru.muctr.part2;
+
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 import static ru.muctr.part2.MyLambdaToMethod.getAreaRectangle;
 
 public class Demo {
     public static void main(String[] args)
     {
-        BinaryOperator <Double> bo = (a, b) -> {if (a > b) return  a; return b; };
-        double[] array = {2.5, 3 , 4.5, 5};
-        System.out.println(lambdaMethod(bo, array));
-        System.out.println(getAreaRectangle(array[0], array[1]));
-        System.out.println(new MyLambdaToMethod().getAreaTriangle(array[2], array[3]));
+        double[] arrayDouble = {2.0, 3.0};
+        BinaryOperator<Double> bo = (a, b) ->  a > b ? a : b ;
+        System.out.println(lambdaMethod(MyLambdaToMethod::getAreaRectangle, arrayDouble));
+        System.out.println(lambdaMethod(bo, arrayDouble));
+        System.out.println(lambdaMethod(new MyLambdaToMethod()::getAreaTriangle, arrayDouble));
+
     }
     public static double lambdaMethod(BinaryOperator<Double> bo, double[] array )
     {
-        return bo.apply(getAreaRectangle(array[0], array[1]), new MyLambdaToMethod().getAreaTriangle(array[2], array[3]));
+        return bo.apply(array[0], array[1]);
     }
 }
